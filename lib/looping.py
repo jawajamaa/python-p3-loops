b8deb13f80c025fae6c291e01df926f776875b09
Timothy Ryon <tim@timryon.com>
2024-03-15 17:57:10 -0400

    wip

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print(f"{i}")
+        i -= 1
+        if i == 0:
+            print("Happy New Year!")
+
+#int_list([1, 2, 3, 4, 5])
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    int_list = [int**2 for int in int_list]
+    return int_list
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    i = 0
+    while i > 100:
+        if i % 3 == 0:
+            print ("Fizz")
+        elif i % 5 == 0:
+            print ("Buzz")
+        elif i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        else:
+            print(f"{i}")
